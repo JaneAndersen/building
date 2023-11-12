0ce9b8682e5aa92587388d3ae73d801ab140da41
JaneAndersen <125027092+JaneAndersen@users.noreply.github.com>
2023-11-12 21:25:20 +0400

    Update Floor.java

--- src/main/java/buildings/Floor.java
+++ src/main/java/buildings/Floor.java
@@ -2,8 +2,8 @@ public interface Floor {
   int getTotalFlats();
   int getFlatsSquare();
   int getFlatsQuantity();
-  Flat[] getFlats();
-  Flat getFlat(int index);
+  Space[] getFlats();
+  Space getFlat(int index);
   void setFlat(int index, Space newFlat);
   void addFlat(int index, Space newFlat);
   void deleteFlat(int index);
