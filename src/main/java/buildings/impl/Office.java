03404173b0eeae9eafdb1788a9ac2673fb996383
JaneAndersen <125027092+JaneAndersen@users.noreply.github.com>
2023-11-17 23:37:58 +0400

    Create Office.java

---
+++ src/main/java/buildings/impl/Office.java
@@ -0,0 +1,30 @@
+package buildings;
+import buildings.Space;
+public class Office extends AbstractSpace implements Space {
+    private int sq,q;
+    public Office() {
+        super();
+    }
+    public Office(int sq) {
+        super(sq);
+    }
+    public Office(int sq, int q) {
+        super(sq,q);
+    }
+    public int getSquare() {
+        return sq;
+    }
+    public
+    void setSquare(int square) {
+        this.sq=square;
+    }
+
+    public int getQuantity() {
+        return q;
+    }
+
+    public void setQuantity(int quantity) {
+
+        this.q=quantity;
+    }
+}
