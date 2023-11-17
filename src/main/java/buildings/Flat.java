fd2fa4a95fe1d91c923da5cbac8f5edfd6bc5932
JaneAndersen <125027092+JaneAndersen@users.noreply.github.com>
2023-11-17 20:02:19 +0400

    Update Flat.java

--- src/main/java/buildings/Flat.java
+++ src/main/java/buildings/Flat.java
@@ -1,32 +1,13 @@
 package buildings;
 import buildings.Space;
-public class Flat implements Space{
-
-    private int square;
-    private int quantity;
-    public static final int SQUARE=50, QUANTITY=2;
+public class Flat extends AbstractSpace implements Space{
     public Flat() {
-        square=50;
-        quantity=2;
+        super();
     }
     public Flat(int sq) {
-        square=sq;
-        quantity=2;
+        super(sq);
     }
     public Flat(int sq, int q) {
-        square=sq;
-        quantity=q;
-    }
-    public int getSquare() {
-        return square;
-    }
-    public void setSquare(int square) {
-        this.square=square;
-    }
-    public int getQuantity() {
-        return quantity;
-    }
-    public void setQuantity(int quantity) {
-        this.quantity=quantity;
+        super(sq,q);
     }
 }
