ca9dc9c2c8bcdebbcc4441df187c27d89a29cea3
JaneAndersen <125027092+JaneAndersen@users.noreply.github.com>
2023-11-17 19:48:05 +0400

    Update AbstractSpace.java

--- src/main/java/buildings/AbstractSpace.java
+++ src/main/java/buildings/AbstractSpace.java
@@ -1,32 +1,20 @@
 package buildings;
-import buildings.Space;
-public class Flat implements Space{
+
+public abstract class AbstractSpace{
 
     private int square;
     private int quantity;
     public static final int SQUARE=50, QUANTITY=2;
-    public Flat() {
+    public AbstractSpace() {
         square=50;
         quantity=2;
     }
-    public Flat(int sq) {
+    public AbsractSpace(int sq) {
         square=sq;
         quantity=2;
     }
-    public Flat(int sq, int q) {
+    public AbsractSpace(int sq, int q) {
         square=sq;
         quantity=q;
-    }
-    public int getSquare() {
-        return square;
-    }
-    public void setSquare(int square) {
-        this.square=square;
-    }
-    public int getQuantity() {
-        return quantity;
-    }
-    public void setQuantity(int quantity) {
-        this.quantity=quantity;
     }
 }
